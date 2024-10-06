4dc42c27bc090536dd7e6b0274cf1aae6dd64eaa
Beatriz Sá <beatrizsa14@gmail.com>
2024-10-06 20:58:39 +0100

    Enhanced visualization function and changes to data analysis

--- utils.py
+++ utils.py
@@ -2,27 +2,46 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import seaborn as sns
 
-def grafic_visualization(data, title, columns, type):
-    if type == 'numerical':
-        columns = columns.drop('Id')
-    if type == 'categorical':
-        columns = columns.drop('Vegetation_Type')
-    fig, axes = plt.subplots((len(columns) + 2) // 3, 3, figsize=(15, 20))
+
+colors = ["#FFB3BA",   "#FFDFBA",   "#FFFFBA",   "#BAFFC9", "#BAE1FF",  "#FFB3E6",  "#B3FFDA",  "#CAB2FF", "#FFB3FF",
+          "#FFC1E3",  "#CCE5FF",  "#B2F0E6",  "#FFD1B2",  "#FFFF99",  "#D1C4E9",   "#FFE0B2",  "#F8BBD0",  "#DCEDC8"]
+
+
+def boxplot_visualization(data, columns, title):
+    n_cols = min(5, len(columns))
+    n_rows = (len(columns) + n_cols - 1) // n_cols
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(15, 5 * n_rows))
 
     axes = axes.flatten()
     for i, col in enumerate(columns):
+        sns.boxplot(data=data, y=col, ax=axes[i], color=colors[i])
 
-        if type == 'numerical':
-            data.boxplot(column=col, by='Vegetation_Type', ax=axes[i])
-        if type == 'categorical':
-            pd.crosstab(data[col], data['Vegetation_Type']).plot(kind='bar', ax=axes[i], stacked=True)
-        axes[i].tick_params(axis='x', rotation=45)
-        axes[i].set_title(col)
-
-    for j in range(i + 1, len(axes)):
+    for j in range(len(columns), len(axes)):
         axes[j].axis('off')
 
-    plt.suptitle(title)
-    plt.tight_layout()
+    plt.tight_layout([0, 0, 1, 0.95])
+    plt.suptitle(title, fontsize=16)
+    plt.show()
+
+
+def barplot_visualization(data, columns, title):
+    n_cols = min(5, len(columns))
+    n_rows = (len(columns) + n_cols - 1) // n_cols
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(15, 5 * n_rows))
+
+    axes = axes.flatten()
+    for i, col in enumerate(columns):
+        label = data.groupby(col).size()
+        sns.barplot(x=label.index, y=label.values, ax=axes[i], color=colors[i])
+        axes[i].set_xlabel(col)
+        axes[i].set_ylabel('Frequency')
+        axes[i].tick_params(axis='x', rotation=45)
+
+    for j in range(len(columns), len(axes)):
+        axes[j].axis('off')
+
+    plt.tight_layout([0, 0, 1, 0.95])
+    plt.suptitle(title, fontsize=16)
     plt.show()
