7e662aadda612c8ab2a1b1d233b11621a58c0e33
Beatriz Sá <beatrizsa14@gmail.com>
2024-10-10 23:10:07 +0100

    Enhanced visualization function and changes to data analysis

--- utils.py
+++ utils.py
@@ -17,11 +17,31 @@ def boxplot_visualization(data, columns, title):
     axes = axes.flatten()
     for i, col in enumerate(columns):
         sns.boxplot(data=data, y=col, ax=axes[i], color=colors[i])
+        axes[i].set_title(col)
+        axes[i].set_ylabel("")
 
     for j in range(len(columns), len(axes)):
         axes[j].axis('off')
 
-    plt.tight_layout([0, 0, 1, 0.95])
+    plt.tight_layout(rect=(0, 0, 1, 0.95))
+    plt.suptitle(title, fontsize=16)
+    plt.show()
+
+def boxplot_by_type_visualization(data, columns, title):
+    n_cols = min(5, len(columns))
+    n_rows = (len(columns) + n_cols - 1) // n_cols
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(15, 5 * n_rows))
+
+    axes = axes.flatten()
+    for i, col in enumerate(columns):
+        sns.boxplot(x='Vegetation_Type', y=col, data=data, ax=axes[i], color=colors[i])
+        axes[i].set_xlabel('Vegetation Type')
+        axes[i].set_ylabel(col)
+
+    for j in range(len(columns), len(axes)):
+        axes[j].axis('off')
+
+    plt.tight_layout(rect=(0, 0, 1, 0.95))
     plt.suptitle(title, fontsize=16)
     plt.show()
 
@@ -42,6 +62,6 @@ def barplot_visualization(data, columns, title):
     for j in range(len(columns), len(axes)):
         axes[j].axis('off')
 
-    plt.tight_layout([0, 0, 1, 0.95])
+    plt.tight_layout(rect=(0, 0, 1, 0.95))
     plt.suptitle(title, fontsize=16)
     plt.show()
