a84873adf3c50b4247bb40596b57432c9afe403f
Beatriz Sá <beatrizsa14@gmail.com>
2024-10-15 21:18:37 +0100

    updated methods application and dataframe to store the metrics

--- README.md
+++ README.md
@@ -1,1 +1,93 @@
-# MachineLearning_AprendizagemAutomática
+# Vegetation Classification using Machine Learning
+
+## Project Overview
+
+The aim of this project is to apply various machine learning methods to a dataset containing environmental and geographical features to classify different types of vegetation. The dataset includes features such as altitude, slope, proximity to water, canopy density, rainfall, and more, each corresponding to a specific region. The target variable is the vegetation type, which consists of three assigned classes representing different ecosystems.
+## Dataset
+
+The dataset contains various features related to geographic and environmental properties of different land regions. Key features include:
+
+- **Altitude** (in meters)
+- **Slope** (in degrees)
+- **Horizontal/Vertical Distance to Water** (in meters)
+- **Distance to Roadways** (in meters)
+- **Shadow Index** (measured at 9h, 12h, 15h)
+- **Canopy Density** (percentage of land covered by trees)
+- **Rainfall in Summer/Winter** (in mm)
+- **Wind Exposure Level** (in Km/h)
+- **Soil Type** (40 different soil types)
+- **Wilderness Area** (4 different wilderness areas)
+- **Vegetation Type** (Target variable with 7 classes)
+
+### Class Distribution
+
+The dataset is imbalanced, with the following distribution:
+
+| Vegetation Type | Type 1 | Type 2 | Type 3 | Type 4 | Type 5 | Type 6 | Type 7 |
+|-----------------|--------|--------|--------|--------|--------|--------|--------|
+| Samples         | 2160   | 1404   | 1620   | 1080   | 1944   | 2160   | 864    |
+
+We will be working with the classes **type 1**,**type 3**,**type 6**.
+
+## Objectives
+
+The project consists of the following main tasks:
+
+1. **Exploratory Data Analysis (EDA)**
+   - Perform descriptive statistics and analyze feature distributions.
+   - Conduct bivariate analysis to explore correlations between features and vegetation types.
+
+2. **Model Training**
+   - Apply the following machine learning methods to the data:
+     - Logistic Regression
+     - Linear Discriminant Analysis (LDA)
+     - Quadratic Discriminant Analysis (QDA)
+   - Use various resampling techniques, including:
+     - Holdout
+     - 5-fold and 10-fold Cross Validation
+     - Leave-One-Out Cross Validation (LOOCV)
+     - Bootstrap
+   - Evaluate models using metrics like accuracy, precision, recall, and F1-score.
+
+3. **Feature Selection**
+   - Investigate if reducing the number of features (via regularization) improves model performance.
+
+## Technologies Used
+- Python 3.11
+- Pandas
+- NumPy
+- Scikit-learn
+- Matplotlib
+- Seaborn
+- Jupyter Notebook
+
+## Project Structure
+
+- **data/**: Contains the data for each assigned class.
+- **notebook.ipynb**: Jupyter notebook for this project.
+- **requirements.txt**: List of dependencies required for running the project.
+- **README.md**: Overview of the project.
+
+## Installation and Setup
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/beatrizmsa/ml-vegetation-classifier.git
+   ```
+
+2. Install required Python packages:
+   ```bash
+   pip install -r requirements.txt
+   ```
+3. Run the Jupyter notebook.
+
+## Results
+
+## Conclusion
+
+## Authors
+
+- [Alexandre Marques - 1240435](https://github.com/AlexandreMarques27)
+- [Beatriz Sá - 1240440](https://github.com/beatrizmsa)
+- [Diogo Gaspar - 1200966](https://github.com/diogogaspar123)
+
--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from sklearn.model_selection import train_test_split, KFold, cross_val_score, LeaveOneOut
+from sklearn.metrics import classification_report
+from sklearn.utils import resample
 
 
 colors = ["#FFB3BA",   "#FFDFBA",   "#FFFFBA",   "#BAFFC9", "#BAE1FF",  "#FFB3E6",  "#B3FFDA",  "#CAB2FF", "#FFB3FF",
@@ -65,3 +68,90 @@ def barplot_visualization(data, columns, title):
     plt.tight_layout(rect=(0, 0, 1, 0.95))
     plt.suptitle(title, fontsize=16)
     plt.show()
+
+
+def add_results(data_results,model_name, method_name, accuracy, precision, recall, f1, std):
+    new_row = {
+        'Model': model_name,
+        'Method': method_name,
+        'Accuracy': accuracy,
+        'Precision': precision,
+        'Recall': recall,
+        'F1': f1,
+        'Std Dev': std
+    }
+    data_results.loc[len(data_results)] = new_row
+
+def holdout_evaluation(data_results,models, X_train, X_test, y_train, y_test):
+    for name, model in models.items():
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        report = classification_report(y_test, y_pred, output_dict=True)
+
+        accuracy = report['accuracy']
+        precision = report['weighted avg']['precision']
+        recall = report['weighted avg']['recall']
+        f1 = report['weighted avg']['f1-score']
+
+        add_results(data_results,name, 'Holdout', accuracy, precision, recall, f1,'Nan')
+
+
+def cross_validation_evaluation(data_results,models,X, y, k_splits):
+    kf = KFold(n_splits=k_splits)
+    for name, model in models.items():
+
+        accuracy_scores = cross_val_score(model, X, y, cv=kf, scoring='accuracy', n_jobs=-1)
+        precision_scores = cross_val_score(model, X, y, cv=kf, scoring='precision_weighted', n_jobs=-1)
+        recall_scores = cross_val_score(model, X, y, cv=kf, scoring='recall_weighted', n_jobs=-1)
+        f1_scores = cross_val_score(model, X, y, cv=kf, scoring='f1_weighted', n_jobs=-1)
+
+        accuracy = accuracy_scores.mean()
+        precision = precision_scores.mean()
+        recall = recall_scores.mean()
+        f1 = f1_scores.mean()
+        std_dev = accuracy_scores.std()
+
+        add_results(data_results,name, f'Cross-Validation with {k_splits}', accuracy, precision, recall, f1, std_dev)
+
+def loocv_evaluation(data_results,models, X, y):
+    cv = LeaveOneOut()
+
+    for name, model in models.items():
+
+        accuracy_scores = cross_val_score(model, X, y, cv=cv, scoring='accuracy', n_jobs=-1)
+        precision_scores = cross_val_score(model, X, y, cv=cv, scoring='precision_weighted', n_jobs=-1)
+        recall_scores = cross_val_score(model, X, y, cv=cv, scoring='recall_weighted', n_jobs=-1)
+        f1_scores = cross_val_score(model, X, y, cv=cv, scoring='f1_weighted', n_jobs=-1)
+
+        accuracy = accuracy_scores.mean()
+        precision = precision_scores.mean()
+        recall = recall_scores.mean()
+        f1 = f1_scores.mean()
+        std_dev = accuracy_scores.std()
+
+        add_results(data_results,name, 'LeaveOneOut', accuracy, precision, recall, f1, std_dev)
+
+def bootstrap_evaluation(data_results,models, X_train, y_train, X_test, y_test, n_iterations=100):
+    bootstrap_scores = {name: {'accuracy': [], 'precision': [], 'recall': [], 'f1': []} for name in models.keys()}
+
+    for i in range(n_iterations):
+        X_resampled, y_resampled = resample(X_train, y_train, n_samples=len(X_train), replace=True)
+
+        for name, model in models.items():
+            model.fit(X_resampled, y_resampled)
+            y_pred = model.predict(X_test)
+            report = classification_report(y_test, y_pred, output_dict=True)
+
+            bootstrap_scores[name]['accuracy'].append(report['accuracy'])
+            bootstrap_scores[name]['precision'].append(report['weighted avg']['precision'])
+            bootstrap_scores[name]['recall'].append(report['weighted avg']['recall'])
+            bootstrap_scores[name]['f1'].append(report['weighted avg']['f1-score'])
+
+    for name, metrics in bootstrap_scores.items():
+        accuracy = np.mean(metrics['accuracy'])
+        precision = np.mean(metrics['precision'])
+        recall = np.mean(metrics['recall'])
+        f1= np.mean(metrics['f1'])
+        std_dev = np.std(metrics['accuracy'])
+
+        add_results(data_results,name, 'Bootstrap', accuracy, precision, recall, f1, std_dev)
